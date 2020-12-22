c20c53be2b6e470f423efb77429e749add45917b
hocrux <hocrux@hocruxui-MacBook-Pro.local>
2020-12-22 23:38:24 +0900

    8.7

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
   const [videos, setVideos] = useState([]); // 빈 배열이 videos에 할당됨
+  const search = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyCK6BW_hCtoAO9d2dKadk6Vciy4yWDTS2U`,
+     requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({...item, id:item.id.videoId })))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  };
 
   useEffect(() => {
     const requestOptions = {
@@ -19,9 +33,12 @@ function App() {
        .then(result => setVideos(result.items)) // setVideos 함수로 videos 업데이트 // console.log(result) result에 items이 포함되어 있고 아이템은 비디오들임
        .catch(error => console.log('error', error));
    }, []);
-   return <VideoList videos={videos} />; //VideoList에 videos객체를 videos에 담아 전달해주고 VideoList에서는 props.videos로 접근이 가능해짐
-   
+   return <div className="styles.app">
+            <SearchHeader onSearch={search}/>
+            <VideoList videos={videos} />; 
+          
+          </div>
  }
- 
+ //Line 36 : VideoList에 videos객체를 videos에 담아 전달해주고 VideoList에서는 props.videos로 접근이 가능해짐
  export default App;
  
