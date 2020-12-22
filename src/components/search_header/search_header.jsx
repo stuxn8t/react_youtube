feb61cdc5e3a43c391f44985908412466621150f
hocrux <hocrux@hocruxui-MacBook-Pro.local>
2020-12-23 01:43:08 +0900

    8.15

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,48 +1,50 @@
 import styles from './search_header.module.css';
-import React, { useRef } from 'react';
+import React, { memo, useRef } from 'react';
 
 
-const SearchHeader = ({ onSearch }) => {
-    const inputRef = useRef();
-    const handleSearch = () => {
-        const value = inputRef.current.value;
-        onSearch(value)
-    }
-
-    const onClick = () => {
-        handleSearch();
-    }
-
-    const onKeyPress = (event) => {
-        if(event.key === 'Enter') {
+const SearchHeader = memo(
+    ({ onSearch }) => {
+        const inputRef = useRef();
+        const handleSearch = () => {
+            const value = inputRef.current.value;
+            onSearch(value)
+        }
+    
+        const onClick = () => {
             handleSearch();
         }
-    } 
-
-    return (
-        <header className={styles.header}>
-            <div className={styles.logo}>
-            <img 
-            lassName={styles.img} 
-            src="/images/logo.png" alt="logo" />
-            <h1 
-            className={styles.title}> Youtube </h1>
-            </div>
-            <input 
-            ref={inputRef}
-            className={styles.input} 
-            type="search" 
-            placeholder="search..." 
-            onKeyPress={onKeyPress}/>
-            <button 
-            className={styles.button} 
-            type="submit" onClick={onClick}>
+    
+        const onKeyPress = (event) => {
+            if(event.key === 'Enter') {
+                handleSearch();
+            }
+        } 
+    
+        return (
+            <header className={styles.header}>
+                <div className={styles.logo}>
                 <img 
-                className={styles.buttonImg} 
-                src="/images/search.png" alt="search" />
-            </button>
-        </header>
-    );
-};
+                lassName={styles.img} 
+                src="/images/logo.png" alt="logo" />
+                <h1 
+                className={styles.title}> Youtube </h1>
+                </div>
+                <input 
+                ref={inputRef}
+                className={styles.input} 
+                type="search" 
+                placeholder="search..." 
+                onKeyPress={onKeyPress}/>
+                <button 
+                className={styles.button} 
+                type="submit" onClick={onClick}>
+                    <img 
+                    className={styles.buttonImg} 
+                    src="/images/search.png" alt="search" />
+                </button>
+            </header>
+        );
+        }
+);
 
 export default SearchHeader;
