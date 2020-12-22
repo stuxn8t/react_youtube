e81264588ff0af89e49adcb39cb7264bd9795224
hocrux <hocrux@hocruxui-MacBook-Pro.local>
2020-12-22 22:33:50 +0900

    8.4

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
-
-const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>;
+const VideoItem = ({ video : { snippet } }) => ( 
+//<h1>{props.video.snippet.title}</h1>;
 // 타이틀을 출력함
+
+<li className={styles.container}>
+    <div className={styles.video}>
+        <img  className={styles.thumbnail} src={snippet.thumbnails.medium.url} 
+        alt="video thumbnail"></img>
+        <div className={styles.metadata}>
+            <p className={styles.title}>{snippet.title}</p>
+            <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
+    </div>
+</li>
+);
 
 export default VideoItem;
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import VideoItem from '../video_item/video_item';
+import styles from './video_list.module.css';
 
 const VideoList = (props) => (
-    <ul>
+    <ul className={styles.videos}>
         {props.videos.map(video => (
         <VideoItem key={video.id} video={video} /> // map함수로 videos에 있는 비디오들을 video에 담아 한바퀴 돌며 VideoItem을 통해 화면에 표시해줌
                                      // map으로 순환된 {video}를 video에 담음 
